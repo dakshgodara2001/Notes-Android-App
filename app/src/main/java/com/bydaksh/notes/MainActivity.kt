43f07dd08d1d4c6c4283a71336a8c15436d463f4
Daksh Godara <godaradaksh2001@gmail.com>
2023-01-03 16:21:22 +0530

    notes-app-commit

---
+++ app/src/main/java/com/bydaksh/notes/MainActivity.kt
@@ -0,0 +1,188 @@
+package com.bydaksh.notes
+
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.ticket.view.*
+
+class MainActivity : AppCompatActivity() {
+    var listNotes=ArrayList<Note>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"onCreate",Toast.LENGTH_LONG).show()
+        LoadQuery("%")
+    }
+
+    override  fun onResume() {
+        super.onResume()
+        LoadQuery("%")
+        Toast.makeText(this,"onResume",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"onStart",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Toast.makeText(this,"onPause",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Toast.makeText(this,"onStop",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Toast.makeText(this,"onDestroy",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this,"onRestart",Toast.LENGTH_LONG).show()
+    }
+
+
+    fun LoadQuery(title:String){
+
+
+
+        var dbManager=DbManager(this)
+        val projections= arrayOf("ID","Title","Description")
+        val selectionArgs= arrayOf(title)
+        val cursor=dbManager.Query(projections,"Title like ?",selectionArgs,"Title")
+        listNotes.clear()
+        if(cursor.moveToFirst()){
+
+            do{
+                val ID=cursor.getInt(cursor.getColumnIndex("ID"))
+                val Title=cursor.getString(cursor.getColumnIndex("Title"))
+                val Description=cursor.getString(cursor.getColumnIndex("Description"))
+
+                listNotes.add(Note(ID,Title,Description))
+
+            }while (cursor.moveToNext())
+        }
+
+        var myNotesAdapter= MyNotesAdpater(this, listNotes)
+        lvNotes.adapter=myNotesAdapter
+
+
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        val sv: SearchView = menu.findItem(R.id.app_bar_search).actionView as SearchView
+
+        val sm= getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        sv.setSearchableInfo(sm.getSearchableInfo(componentName))
+        sv.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(applicationContext, query, Toast.LENGTH_LONG).show()
+                LoadQuery("%$query%")
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item != null) {
+            when(item.itemId){
+                R.id.addNote->{
+                    //Got to add page
+                    var intent= Intent(this,AddNotes::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    inner class  MyNotesAdpater:BaseAdapter{
+        var listNotesAdpater=ArrayList<Note>()
+        var context:Context?=null
+        constructor(context:Context, listNotesAdpater:ArrayList<Note>):super(){
+            this.listNotesAdpater=listNotesAdpater
+            this.context=context
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+
+            var myView=layoutInflater.inflate(R.layout.ticket,null)
+
+            var myNote=listNotesAdpater[p0]
+            myView.tvTitle.text=myNote.nodeName
+
+            myView.tvDes.text=myNote.nodeDes
+            myView.ivDelete.setOnClickListener{
+                var dbManager=DbManager(this.context!!)
+                val selectionArgs= arrayOf(myNote.nodeID.toString())
+                dbManager.Delete("ID=?",selectionArgs)
+                LoadQuery("%")
+            }
+            myView.ivEdit.setOnClickListener{
+
+                GoToUpdate(myNote)
+
+            }
+            return myView
+        }
+
+        override fun getItem(p0: Int): Any {
+            return listNotesAdpater[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getCount(): Int {
+
+            return listNotesAdpater.size
+
+        }
+
+
+
+    }
+
+
+    fun GoToUpdate(note:Note){
+        var intent=  Intent(this,AddNotes::class.java)
+        intent.putExtra("ID",note.nodeID)
+        intent.putExtra("name",note.nodeName)
+        intent.putExtra("des",note.nodeDes)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/bydaksh/notes/Note.kt
@@ -0,0 +1,16 @@
+package com.bydaksh.notes
+
+
+class Note{
+
+    var nodeID:Int?=null
+    var nodeName:String?=null
+    var nodeDes:String?=null
+
+    constructor(nodeID:Int,nodeName:String,nodeDes:String){
+        this.nodeID=nodeID
+        this.nodeName=nodeName
+        this.nodeDes=nodeDes
+    }
+
+}
